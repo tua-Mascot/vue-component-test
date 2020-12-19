110fb1c8c88beef428f24406a77c2afeb61e27f7
tua.Mascot (Kirill P.) <tua.Mascot@mail.com>
2020-12-19 22:05:27 +0300

    add

--- src/components/Conditions.vue
+++ src/components/Conditions.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="component">
     <div class="component__header">
-      <p>Conditions</p>
+      <p>{{ displaySettings.subType }}</p>
     </div>
     <div class="component__title">
-      <p> > Followers</p>
+      <p>{{ displaySettings.type }}</p>
     </div>
     <div class="component__mBody">
       <div class="mBody__options">
-        <p>Follow</p>
+        <p>{{ checkingComponentTitle }}</p>
       </div>
       <div class="mBody__values">
-        <div class="values__opt"></div>
+        <div class="values__opt"><input type="text"> {{elements}}</div>
+        <!-- {{ dataChecker() }} -->
+        <div class="values__addBtn">
+            <button>+ Add value</button>
+        </div>
       </div>
     </div>
     <div class="component__bottom">
@@ -36,7 +40,7 @@ export default {
         {
           type: 'rule',
           condition: {
-	  entity: 'contact',
+            entity: 'contact',
             field: 'followers',
             operand: 'lt',
             value: 5000,
@@ -46,33 +50,77 @@ export default {
           },
           onMatch: {
             action: 'goto',
-            target: '5e26e98027b2c58a70085d4f',
+            target: '5e26e98027b2c58a1f',
           },
         },
         {
           type: 'rule',
           condition: {
-	  entity: 'contact',
+            entity: 'contact',
             field: 'followers',
             operand: 'lt',
-            value: 15000,
+            value: 10000,
           },
           onFail: {
             action: 'goto',
-            target: '5e26e98027b2c58a70085d4e',
+            target: '5e26e98027b2c58a2e',
           },
           onMatch: null,
         },
       ],
+      valueArr: [],
     };
   },
+  computed: {
+    checkingComponentTitle() {
+      if (this.displaySettings.type === 'followers') {
+        return 'Followers count is Greater than';
+      }
+      return 'Err';
+    },
+  },
+  // methods: {
+  //   dataChecker() {
+  //     // return (console.log(typeof (this.elements)));
+  //     return this.elements.forEach(1);
+  //   },
+  // },
 };
+
+// methods: {
+
+// eslint-disable-next-line consistent-return
+//   dataChecker() {
+//     for (let i = 0; i < this.elements.length; i += 1) {
+//       if (this.elements[i] >= 0) {
+//         return `<div class="values__opt">${this.elements[i].value}</div>`;
+//       }
+//       return '<div class="values__opt">Err</div>';
+//     }
+//   },
+// },
+// methods: {
+//   dataChecker() {
+//     for (let i = 0; i < this.elements.length; i += 1) {
+//       if (this.elements[i] >= 0) {
+//         this.valueArr.push(this.elements[i].value);
+//         return this.valueArr;
+//       }
+//       this.valueArr.push('Err');
+//       return this.valueArr;
+//     }
+//   },
+// },
+// watch: {
+// },
+// };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
 .component {
   min-width: 225px;
+  max-width: 225px;
   min-height: 275px;
   margin: 12px;
   background-color: #ffffff;
@@ -92,6 +140,9 @@ export default {
     p {
       padding: 0 10px;
     }
+    p:first-letter {
+    text-transform: capitalize;
+    }
 }
 
 .component__title {
@@ -100,10 +151,13 @@ export default {
   width: 100%;
   min-height: 34px;
   border-bottom: 1px solid #d1d1d1;
-  p {
+    p {
       padding: 0 10px;
       color: #818181;
       font-size: 9px;
+     }
+    p:first-letter {
+    text-transform: capitalize;
     }
 }
 
@@ -131,6 +185,30 @@ export default {
   flex-direction: column;
   width: 35%;
   min-height: 34px;
+}
+
+.values__opt {
+  width: 100%;
+  min-height: 34px;
+  border: 1px #d1d1d1;
+  border-style: none dashed dashed;
+  input {
+    width: 100%;
+    min-height: 34px;
+    border: none;
+  }
+}
+
+.values__addBtn {
+  width: 100%;
+  min-height: 34px;
+  border: 1px #d1d1d1;
+  border-style: none dashed dashed;
+  button {
+    width: 100%;
+    min-height: 34px;
+    border: none;
+  }
 }
 
 .component__bottom {
