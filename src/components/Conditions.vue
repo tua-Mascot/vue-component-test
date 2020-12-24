851ff2319ca801a2ff7e2d555634f865a2dfa8e4
tua.Mascot (Kirill P.) <tua.Mascot@mail.com>
2020-12-24 19:57:46 +0300

    add

--- src/components/Conditions.vue
+++ src/components/Conditions.vue
@@ -1,7 +1,9 @@
 <template>
   <div class='component'>
     <div class='component__header'>
+      <img :src='require( `@/assets/logo.png`)' width='16' height='16' alt='Vue'/>
       <p>{{ displaySettings.subType }}</p>
+      <div class='decorativeCircleLeft'></div>
     </div>
     <div class='component__title'>
       <p>{{ displaySettings.type }}</p>
@@ -11,20 +13,28 @@
         <p>{{ checkingComponentTitle }}</p>
       </div>
       <div class='mBody__values'>
-        <div class='values__opt' v-for='(element, index) of elements' :key='index'>
-          <button @click='removeElement(index)'>&times;</button>
-          <input v-model.number='element.condition.value' />
+         <div class='values__opt' v-for='(element, index) of elements' :key='index'> <!-- key id -->
+          <button :disabled='elements.length == 1' @click='removeElement(index)'
+          :style='{opacity: elements.length > 1 ? 1 : 0}'>&times;</button>
+          <input v-model='element.condition.value' />
+          <div class='decorativeCircleRight'></div>
         </div>
         <div class='values__addBtn'>
             <button @click='addValue()'>+ Add value</button>
+            <div class='decorativeCircleRight'></div>
         </div>
       </div>
     </div>
     <div class='component__bottom'>
       <div class='bottom__title'>
-        <p>Follow is</p>
+        <p>Followers count is</p>
       </div>
-      <div class='bottom__totalValue'></div>
+      <div class='bottom__wrap'>
+        <div class='bottom__totalValue'>
+          <p>5000 or </p>
+        </div>
+        <div class='decorativeCircleRight'></div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,7 +43,7 @@
 export default {
   name: 'conditions',
   data() {
-    return { // : ( ) return ?????
+    return {
       displaySettings: {
         subType: 'condition',
         type: 'followers',
@@ -57,18 +67,20 @@ export default {
           },
         },
       ],
+      firstOnFailSaver: null,
     };
   },
   mounted() {
-    const firstOnFailSaver = JSON.parse(JSON.stringify(this.elements[0]));
-    console.log(firstOnFailSaver);
-    console.log(this.elements.length);
-    return firstOnFailSaver;
+    this.firstOnFailSaver = JSON.parse(JSON.stringify(this.elements[0]));
   },
   computed: {
     checkingComponentTitle() {
       if (this.displaySettings.type === 'followers') {
-        return 'Followers count is Greater than';
+        return `Followers count is${<span class='dropdown'>Greater
+        <div class='dropdown__content'>
+        <span>Greater</span><span>Less</span>
+        </div>
+        </span>}than`;
       }
       return 'Err';
     },
@@ -80,10 +92,8 @@ export default {
     addValue() {
       const newValue = JSON.parse(JSON.stringify(this.lastItemElements)); // Why not a func? this?
       newValue.onMatch = null;
-      // newValue.onFail = this.elements[this.elements.length - 1].onFail;
       this.elements[this.elements.length - 1].onFail = 'fallthrough';
       this.elements.push(newValue);
-      console.log(this.elements);
     },
     removeElement(index) {
       this.elements.splice(index, 1);
@@ -93,9 +103,9 @@ export default {
   watch: {
     elements: {
       deep: true,
-      handler(newElements, oldElements) {
-        if (newElements.length === 1 || oldElements.length === 1) {
-          this.elements[0].onFail = 5555;
+      handler(newElements) {
+        if (newElements.length === 1) {
+          this.elements[0].onFail = this.firstOnFailSaver;
           console.log(this.elements);
         }
       },
@@ -107,29 +117,35 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss'>
   .component {
+    display: flex;
+    flex-direction: column;
     min-width: 225px;
     max-width: 225px;
     min-height: 275px;
     margin: 12px;
     background-color: #ffffff;
-    border-radius: 12px;
+    border-radius: 6px;
+    border: 1px solid #41b883;
     box-shadow: 3px 3px 3px #dddddd;
+    /* overflow: hidden; */
   }
 
   .component__header {
+    position: relative;
     display: flex;
     align-items: center;
     width: 100%;
     min-height: 34px;
     /* max-height: 42px; */
-    background-color: #16965a;
-    color: #ffffff;
-    border-radius: 12px 12px 0 0;
+    background-color: #bae7d3;
       p {
-        padding: 0 10px;
+        padding: 0 10px 0 0;
       }
       p:first-letter {
         text-transform: capitalize;
+      }
+      img {
+        margin: 0 6px;
       }
   }
 
@@ -138,7 +154,8 @@ export default {
     align-items: center;
     width: 100%;
     min-height: 34px;
-    border-bottom: 1px solid #d1d1d1;
+    border: 1px solid #d1d1d1;
+    border-style: solid none;
       p {
         padding: 0 10px;
         color: #818181;
@@ -151,8 +168,8 @@ export default {
 
   .component__mBody {
     display: flex;
-    align-items: center;
     width: 100%;
+    height: 100%;
     min-height: 34px;
   }
 
@@ -161,10 +178,26 @@ export default {
     align-items: center;
     width: 65%;
     min-height: 34px;
+    border: 1px #d1d1d1;
+    border-style: none dashed none none;
       p {
         padding: 0 10px;
         color: #818181;
       }
+  }
+
+  .dropdown {
+    position: relative;
+    :hover {
+      cursor: pointer;
+    }
+  }
+
+  .dropdown__content {
+    z-index: 1;
+    display: none;
+    position: absolute;
+    background-color: #f1f1f1;
   }
 
   .mBody__values {
@@ -172,45 +205,59 @@ export default {
     align-items: center;
     flex-direction: column;
     width: 35%;
-    min-height: 34px;
+    /* min-height: 34px; */
   }
 
   .values__opt {
+    position: relative;
     display: flex;
     width: 100%;
     min-height: 34px;
     border: 1px #d1d1d1;
-    border-style: none dashed dashed;
+    border-style: none none dashed none;
       input {
-        width: 100%;
+        width: 80%;
         min-height: 34px;
-        padding: 0 10px;
+        padding: 0 0 0 5px;
         border: none;
         outline: none;
+        font-size: 12px;
+          /* &:focus {
+            border: 1px solid #41b883;
+          } */
       }
       button {
+        width: 20%;
         border: none;
         background: none;
       }
   }
 
   .values__addBtn {
+    position: relative;
     width: 100%;
     min-height: 34px;
-    border: 1px #d1d1d1;
-    border-style: none dashed dashed;
+    margin-top: auto;
+    /* border: 1px #d1d1d1;
+    border-style: none none none dashed; */
       button {
+        color: #41b883;
         width: 100%;
         min-height: 34px;
         border: none;
         background: none;
+        font-size: 12px;
       }
   }
 
   .component__bottom {
     display: flex;
+    align-self: flex-end;
     width: 100%;
     min-height: 34px;
+    margin-top: auto;
+    border: 1px #d1d1d1;
+    border-style: dashed none none none;
   }
 
   .bottom__title {
@@ -218,10 +265,16 @@ export default {
     align-items: center;
     width: 65%;
     min-height: 34px;
+    border: 1px #d1d1d1;
+    border-style: none dashed none none;
       p {
         padding: 0 10px;
         color: #818181;
       }
+  }
+
+  .bottom__wrap {
+    position: relative;
   }
 
   .bottom__totalValue {
@@ -229,5 +282,32 @@ export default {
     align-items: center;
     width: 35%;
     min-height: 34px;
+      p {
+        padding-left: 20px;
+      }
+    /* border: 1px #d1d1d1;
+    border-style: none none none dashed; */
+  }
+
+  .decorativeCircleLeft {
+    /* z-index: 1; */
+    position: absolute;
+    left: -5px;
+    height: 10px;
+    width: 10px;
+    border-radius: 50%;
+    border: 1px solid #41b883;
+    background-color: #ffffff;
+  }
+
+  .decorativeCircleRight {
+    position: absolute;
+    top: 12px;
+    left: 73px;
+    height: 10px;
+    width: 10px;
+    border-radius: 50%;
+    border: 1px solid #41b883;
+    background-color: #ffffff;
   }
 </style>
