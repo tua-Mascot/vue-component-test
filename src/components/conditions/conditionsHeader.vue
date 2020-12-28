84bc64b54a92fe5172e62896009ad0cf4f1920de
tua.Mascot (Kirill P.) <tua.Mascot@mail.com>
2020-12-28 19:57:38 +0300

    test

--- src/components/conditions/conditionsHeader.vue
+++ src/components/conditions/conditionsHeader.vue
@@ -9,29 +9,42 @@
     <div class="conditions__title">
       <!-- <p>{{ displaySettings.type }}</p> -->
       <p>
-        <select v-model="conditions__titles_selected">
+        <select v-model="conditions_data"> <!-- v-model="conditions__titles_selected" -->
           <option v-for="conditions__title of conditions__titles"
-          :key="conditions__title" :value="conditions__title">
+          :key="conditions__title">  <!-- :value="conditions__title" -->
           {{ conditions__title }}
           </option>
         </select>
       </p>
     </div>
-    <conditionsBodyFollowers />
+    <!-- <conditionsBodyFollowers /> -->
+    <!-- <conditionsBodyFollowers v-if="this.conditions_data === 'followers'" />
+    <conditionsBodyLikes v-else-if="this.conditions_data === 'likes'" />
+    <conditionsBodyEtc v-else-if="this.conditions_data === 'etc'" /> -->
+    <!-- <component v-else>Error</component> -->
+    <component v-bind:is="conditions_data"></component>
   </div>
 </template>
 
 <script>
 import conditionsBodyFollowers from './conditionsBodyFollowers.vue';
+import conditionsBodyLikes from './conditionsBodyFollowers.vue';
+import conditionsBodyEtc from './conditionsBodyFollowers.vue';
 
 export default {
   name: 'conditionsHeader',
   components: {
+    // conditionsBodyFollowers: () => import('./conditionsBodyFollowers.vue'),
+    // conditionsBodyLikes: () => import('./conditionsBodyFollowers.vue'),
+    // conditionsBodyEtc: () => import('./conditionsBodyFollowers.vue'),
     conditionsBodyFollowers,
+    conditionsBodyLikes,
+    conditionsBodyEtc,
   },
   data() {
     return {
-      conditions__titles_selected: 'followers',
+      conditions_data: '',
+      conditions__titles_current: '',
       conditions__titles: [
         'followers',
         'likes',
@@ -39,7 +52,11 @@ export default {
       ],
     };
   },
+  beforeUpdate() {
+    console.log(this.conditions_data);
+  },
 };
+
 </script>
 
 <style lang='scss'>
