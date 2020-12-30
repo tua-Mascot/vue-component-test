0eb1ab3015fdf01beb3845161a015cc2b83ef44f
tua.Mascot (Kirill P.) <tua.Mascot@mail.com>
2020-12-30 19:25:04 +0300

    conditionsHeader BEM fix

--- src/components/conditions/conditionsHeader.vue
+++ src/components/conditions/conditionsHeader.vue
@@ -4,35 +4,31 @@
       <img :src="require( `@/assets/logo.png`)" width="16" height="16" alt="Vue"/>
       <!-- <p>{{ displaySettings.subType }}</p> -->
       <!-- <p v-if=""></p> -->
-      <div class="decorativeCircleLeft"></div>
+      <div class="decorative-circle--left"></div>
     </div>
     <div class="conditions__title">
       <!-- <p>{{ displaySettings.type }}</p> -->
       <p>
-        <select v-model="conditions_data"> <!-- v-model="conditions__titles_selected" -->
-          <option v-for="conditions__title of conditions__titles"
-          :key="conditions__title">  <!-- :value="conditions__title" -->
-          {{ conditions__title }}
+        <select v-model="conditionsData"> <!-- v-model="conditions__titles_selected" -->
+          <option v-for="conditionsTitle of conditionsTitles"
+          :key="conditionsTitle">  <!-- :value="conditions__title" -->
+          {{ conditionsTitle }}
           </option>
         </select>
       </p>
     </div>
-    <!-- <conditionsBodyFollowers /> -->
+    <conditionsBodyFollowers />
     <component v-if="component" :is="conditionsBodyFollowers"></component>
   </div>
 </template>
 
 <script>
 import conditionsBodyFollowers from './conditionsBodyFollowers.vue';
-// import conditionsBodyLikesFake from './conditionsBodyLikesFake.vue';
-// import conditionsBodyEtcFake from './conditionsBodyEtcFake.vue';
 
 export default {
   name: 'conditionsHeader',
   components: {
     // conditionsBodyFollowers: () => import('./conditionsBodyFollowers.vue'),
-    // conditionsBodyLikes: () => import('./conditionsBodyFollowers.vue'),
-    // conditionsBodyEtc: () => import('./conditionsBodyFollowers.vue'),
     conditionsBodyFollowers,
     // conditionsBodyLikesFake,
     // conditionsBodyEtcFake,
@@ -40,8 +36,8 @@ export default {
   data() {
     return {
       component: null,
-      conditions_data: '',
-      conditions__titles: [
+      conditionsData: '',
+      conditionsTitles: [
         'followers',
         'likes',
         'etc',
@@ -54,7 +50,7 @@ export default {
     },
   },
   watch: {
-    conditions_data: {
+    conditionsData: {
       handler(value) {
         if (value === 'followers') {
           this.element().then(() => {
@@ -83,45 +79,44 @@ export default {
     border: 1px solid #41b883;
     box-shadow: 3px 3px 3px #dddddd;
     /* overflow: hidden; */
+      &__header {
+        position: relative;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        min-height: 34px;
+        background-color: #bae7d3;
+        border-radius: 5px 5px 0 0;
+          p {
+            padding: 0 10px 0 0;
+          }
+          p:first-letter {
+            text-transform: capitalize;
+          }
+          img {
+            margin: 0 6px;
+          }
+      }
+
+      &__title {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        min-height: 34px;
+        border: 1px solid #d1d1d1;
+        border-style: solid none;
+          p {
+            padding: 0 10px;
+            color: #818181;
+            font-size: 9px;
+          }
+          p:first-letter {
+            text-transform: capitalize;
+          }
+      }
   }
 
-  .conditions__header {
-    position: relative;
-    display: flex;
-    align-items: center;
-    width: 100%;
-    min-height: 34px;
-    background-color: #bae7d3;
-    border-radius: 5px 5px 0 0;
-      p {
-        padding: 0 10px 0 0;
-      }
-      p:first-letter {
-        text-transform: capitalize;
-      }
-      img {
-        margin: 0 6px;
-      }
-  }
-
-  .conditions__title {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    min-height: 34px;
-    border: 1px solid #d1d1d1;
-    border-style: solid none;
-      p {
-        padding: 0 10px;
-        color: #818181;
-        font-size: 9px;
-      }
-      p:first-letter {
-        text-transform: capitalize;
-      }
-  }
-
-  .decorativeCircleLeft {
+  .decorative-circle--left {
     /* z-index: 999;
     clear: both;
     overflow: visible; */
