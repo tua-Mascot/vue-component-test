5fe14281feda0466ed7c93859ef5ab42e0a1aa51
tua.Mascot (Kirill P.) <tua.Mascot@mail.com>
2020-12-30 19:42:27 +0300

    conditionsBodyFollowers BEM fix

--- src/components/conditions/conditionsBodyFollowers.vue
+++ src/components/conditions/conditionsBodyFollowers.vue
@@ -1,7 +1,7 @@
 <template>
-   <div class="conditions__mBody">
-      <div class="mBody__options">
-        <div class="dropdown__content">
+   <div class="conditions__m-body">
+      <div class="conditions__m-body-options">
+        <div class="conditions__m-body-options-dropdown">
           <p>
             Followers count <br> is
             <select v-model="selected">
@@ -12,28 +12,29 @@
           </p>
         </div>
       </div>
-      <div class="mBody__values">
-         <div class="values__opt" v-for="(element, index) of elements" :key="index"> <!-- key id -->
+      <div class="conditions__m-body-values">
+         <div class="conditions__m-body-values-opt"
+         v-for="(element, index) of elements" :key="index">
           <button :disabled="elements.length === 1" @click="removeElement(index)"
           :style="{opacity: elements.length > 1 ? 1 : 0}">&times;</button>
           <input v-model="element.condition.value" />
-          <div class="decorativeCircleRight"></div>
+          <div class="decorative-circle--right"></div>
         </div>
-        <div class="values__addBtn">
+        <div class="conditions__m-body-values-add-btn">
             <button @click="addValue()">+ Add value</button>
-            <div class="decorativeCircleRight"></div>
+            <div class="decorative-circle--right"></div>
         </div>
       </div>
     </div>
     <div class="conditions__bottom">
-      <div class="bottom__title">
+      <div class="conditions__bottom-title">
         <p>Followers count is</p>
       </div>
-      <div class="bottom__wrap">
-        <div class="bottom__totalValue">
+      <div class="conditions__bottom-total-value--bottom-wrap">
+        <div class="conditions__bottom-total-value">
           <p>5000 or <br><span>{{ checkingSelected }}</span></p>
         </div>
-        <div class="decorativeCircleRight"></div>
+        <div class="decorative-circle--right"></div>
       </div>
     </div>
 </template>
@@ -70,9 +71,6 @@ export default {
       selected: 'Greater',
     };
   },
-  // props: {
-  //   title: {},
-  // },
   mounted() {
     this.firstOnFailSaver = JSON.parse(JSON.stringify(this.elements[0]));
   },
@@ -85,18 +83,6 @@ export default {
       console.log(2);
       return 'greater';
     },
-    // checkingConditionsTitle() {
-    //   // TODO
-    //   if (this.displaySettings.type === 'followers') {
-    //     return `Followers count is${<span class='dropdown'>Greater
-    //     <div class='dropdown__content'>
-    //     <span>Greater</span><span>Less</span>
-    //     </div>
-    //     </span>}than`;
-    //   }
-    //   return 'Err';
-    // },
-
   },
   methods: {
     addValue() {
@@ -127,133 +113,130 @@ export default {
 </script>
 
 <style lang='scss'>
- .conditions__mBody {
-    display: flex;
-    width: 100%;
-    height: 100%;
-    min-height: 34px;
-  }
-
-  .mBody__options {
-    display: flex;
-    align-items: center;
-    width: 65%;
-    min-height: 34px;
-    border: 1px #d1d1d1;
-    border-style: none dashed none none;
-      p {
-        padding: 0 10px;
-        color: #818181;
-      }
-  }
-
-  .dropdown__content {
-    position: absolute;
-      select {
-        width: 50px;
-        font-size: 14px;
-        border: none;
-        -webkit-appearance: none;
-        -moz-appearance: none;
-        appearance: none;
-        cursor: pointer;
-          &:focus {
-            background-color: #bae7d3
+  .conditions {
+    &__m-body {
+      display: flex;
+      width: 100%;
+      height: 100%;
+      min-height: 34px;
+    }
+    &__m-body-options {
+      display: flex;
+      align-items: center;
+      width: 65%;
+      min-height: 34px;
+      border: 1px #d1d1d1;
+      border-style: none dashed none none;
+        p {
+          padding: 0 10px;
+          color: #818181;
         }
-      }
-  }
-
-  .mBody__values {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    width: 35%;
-    min-height: 34px;
-  }
-
-  .values__opt {
-    position: relative;
-    display: flex;
-    width: 100%;
-    min-height: 34px;
-    border: 1px #d1d1d1;
-    border-style: none none dashed none;
-      input {
-        color: #41b883;
-        width: 80%;
-        min-height: 34px;
-        padding: 0 0 0 5px;
-        border: none;
-        outline: none;
-        font-size: 12px;
-          &:focus {
-            text-decoration: underline;
+    }
+    &__m-body-options-dropdown {
+      position: absolute;
+        select {
+          width: 50px;
+          font-size: 14px;
+          border: none;
+          -webkit-appearance: none;
+          -moz-appearance: none;
+          appearance: none;
+          cursor: pointer;
+            &:focus {
+              background-color: #bae7d3
           }
-      }
-      button {
-        width: 20%;
-        border: none;
-        background: none;
-      }
+        }
+    }
+    &__m-body-values {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      width: 35%;
+      min-height: 34px;
+    }
+    &__m-body-values-opt {
+      position: relative;
+      display: flex;
+      width: 100%;
+      min-height: 34px;
+      border: 1px #d1d1d1;
+      border-style: none none dashed none;
+        input {
+          color: #41b883;
+          width: 80%;
+          min-height: 34px;
+          padding: 0 0 0 5px;
+          border: none;
+          outline: none;
+          font-size: 12px;
+            &:focus {
+              text-decoration: underline;
+            }
+        }
+        button {
+          width: 20%;
+          border: none;
+          background: none;
+        }
+    }
+    &__m-body-values-add-btn {
+      position: relative;
+      width: 100%;
+      min-height: 34px;
+      margin-top: auto;
+      /* border: 1px #d1d1d1;
+      border-style: none none none dashed; */
+        button {
+          color: #41b883;
+          width: 100%;
+          min-height: 34px;
+          border: none;
+          background: none;
+          font-size: 12px;
+        }
+    }
+
+    &__bottom {
+      display: flex;
+      width: 100%;
+      min-height: 34px;
+      margin-top: auto;
+      border: 1px #d1d1d1;
+      border-style: dashed none none none;
+    }
+
+    &__bottom-title {
+      display: flex;
+      align-items: center;
+      width: 65%;
+      min-height: 34px;
+      border: 1px #d1d1d1;
+      border-style: none dashed none none;
+        p {
+          padding: 0 10px;
+          color: #818181;
+        }
+    }
+
+    &__bottom-total-value--bottom-wrap {
+      position: relative;
+      width: 35%;
+    }
+
+    &__bottom-total-value {
+      display: flex;
+      align-items: center;
+      margin: 0;
+      /* width: 35%; */
+      min-height: 34px;
+        p {
+          color: #b84141;
+          padding-left: 4px;
+        }
+    }
   }
 
-  .values__addBtn {
-    position: relative;
-    width: 100%;
-    min-height: 34px;
-    margin-top: auto;
-    /* border: 1px #d1d1d1;
-    border-style: none none none dashed; */
-      button {
-        color: #41b883;
-        width: 100%;
-        min-height: 34px;
-        border: none;
-        background: none;
-        font-size: 12px;
-      }
-  }
-
-  .conditions__bottom {
-    display: flex;
-    width: 100%;
-    min-height: 34px;
-    margin-top: auto;
-    border: 1px #d1d1d1;
-    border-style: dashed none none none;
-  }
-
-  .bottom__title {
-    display: flex;
-    align-items: center;
-    width: 65%;
-    min-height: 34px;
-    border: 1px #d1d1d1;
-    border-style: none dashed none none;
-      p {
-        padding: 0 10px;
-        color: #818181;
-      }
-  }
-
-  .bottom__wrap {
-    position: relative;
-    width: 35%;
-  }
-
-  .bottom__totalValue {
-    display: flex;
-    align-items: center;
-    margin: 0;
-    /* width: 35%; */
-    min-height: 34px;
-      p {
-        color: #b84141;
-        padding-left: 4px;
-      }
-  }
-
-  .decorativeCircleRight {
+  .decorative-circle--right {
     position: absolute;
     top: 12px;
     left: 73px;
